e8dec56d2d1287b06a82141c54e45436fcaf7d29
Justin Hileman <justin@justinhileman.info>
2016-06-14 22:01:24 -0700

    Update for the sane new world of OMF keybindings!
    See #260 :)

--- functions/fish_user_key_bindings.fish
+++
@@ -1,4 +0,0 @@
-function fish_user_key_bindings
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
---
+++ key_bindings.fish
@@ -0,0 +1,2 @@
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments

b5b9115441b3178a2607b3e4df81e149f1b96f4d
Peter Ho <mail@petertriho.com>
2021-01-18 20:21:52 +1100

    remove top layer .fish file to support fisher 4.2.0

--- key_bindings.fish
+++
@@ -1,7 +0,0 @@
-if not test "$fish_key_bindings" = 'fish_default_key_bindings'
-    bind -M insert ! __history_previous_command
-    bind -M insert '$' __history_previous_command_arguments
-else
-    bind ! __history_previous_command
-    bind '$' __history_previous_command_arguments
-end

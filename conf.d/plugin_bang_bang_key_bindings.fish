202f5b68f28a242e61480c7a69a7631ce25403e2
Peter Ho <mail@petertriho.com>
2021-01-29 20:06:48 +1100

    remove bindings that broke end of line in fish vi -> normal mode

--- conf.d/plugin_bang_bang_key_bindings.fish
+++ conf.d/plugin_bang_bang_key_bindings.fish
@@ -1,4 +1,2 @@
 bind -M insert ! __history_previous_command
 bind -M insert '$' __history_previous_command_arguments
-bind ! __history_previous_command
-bind '$' __history_previous_command_arguments

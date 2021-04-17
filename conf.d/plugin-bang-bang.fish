51f499e37dd88883f36bb875eb5ea4fb058dc7ce
Peter Tri Ho <mail@petertriho.com>
2021-04-17 17:58:14 +1000

    fix "$" binding not working when in normal mode with fish vi key bindings

--- conf.d/plugin-bang-bang.fish
+++ conf.d/plugin-bang-bang.fish
@@ -1,10 +1,9 @@
-bind ! __history_previous_command
-bind '$' __history_previous_command_arguments
-
-# set up the same key bindings for insert mode if using fish_vi_key_bindings
 if test "$fish_key_bindings" != fish_default_key_bindings
     bind --mode insert ! __history_previous_command
     bind --mode insert '$' __history_previous_command_arguments
+else
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
 end
 
 function _plugin-bang-bang_uninstall --on-event plugin-bang-bang_uninstall

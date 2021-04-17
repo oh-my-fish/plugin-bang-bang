b22ac529211ff7245dee61a3dc01024ee5d4d415
Peter Tri Ho <mail@petertriho.com>
2021-04-17 17:49:40 +1000

    add support for hybrid fish key bindings

--- conf.d/plugin-bang-bang.fish
+++ conf.d/plugin-bang-bang.fish
@@ -2,7 +2,7 @@ bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
 
 # set up the same key bindings for insert mode if using fish_vi_key_bindings
-if test "$fish_key_bindings" = 'fish_vi_key_bindings'
+if test "$fish_key_bindings" != fish_default_key_bindings
     bind --mode insert ! __history_previous_command
     bind --mode insert '$' __history_previous_command_arguments
 end

0c4351aa7c8e3b02b7679af855d75ac1f7a7609f
manos00 <manos.wagner@gmail.com>
2022-01-09 01:16:40 +0100

    added prev command as root functionality

--- conf.d/plugin-bang-bang.fish
+++ conf.d/plugin-bang-bang.fish
@@ -1,14 +1,18 @@
 bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
+bind 'S' __history_previous_command_root
 
 # set up the same key bindings for insert mode if using fish_vi_key_bindings
 if test "$fish_key_bindings" = 'fish_vi_key_bindings'
     bind --mode insert ! __history_previous_command
     bind --mode insert '$' __history_previous_command_arguments
+    bind --mode insert S __history_previous_command_root
+
 end
 
 function _plugin-bang-bang_uninstall --on-event plugin-bang-bang_uninstall
     bind --erase --all !
     bind --erase --all '$'
+    bind --erase --all S
     functions --erase _plugin-bang-bang_uninstall
 end

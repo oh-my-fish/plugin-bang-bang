da1618eb20e6c4283e89e24ea045bf0fea5e2996
Alexander Kuntsch <github@akuntsch.de>
2023-01-18 14:21:16 +0100

    Fix issues on multiline commandline buffers

--- functions/__history_previous_command.fish
+++ functions/__history_previous_command.fish
@@ -1,8 +1,9 @@
 function __history_previous_command
-  switch (commandline -t)
+  switch (commandline --current-token)[-1]
   case "!"
-    commandline -t $history[1]; commandline -f repaint
+    commandline --current-token $history[1]
+    commandline --function repaint
   case "*"
-    commandline -i !
+    commandline --insert !
   end
 end
